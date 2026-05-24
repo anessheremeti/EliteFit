da4d0157084cfa8bd172b88df3df25648c4bb93e
Lendrit11 <lendritl122@gmail.com>
2026-05-24 02:46:01 +0200

    feat: implement video upload and filtered retrieval for workout videos

---
+++ EliteBackend/EliteFit.Api/Controllers/WorkoutsCon/WorkoutsController.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using EliteFit.Application.Features.Queries.Workouts;
+using EliteFit.Application.Features.Commands.Workouts;
+
+namespace EliteFit.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WorkoutsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public WorkoutsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("videos")]
+        public async Task<IActionResult> GetVideos([FromQuery] GetWorkoutVideosQuery query)
+        {
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpPost("videos")]
+         public async Task<IActionResult> CreateVideo([FromForm] CreateWorkoutVideoCommand command)
+        {
+            // command.UploaderId duhet të jetë marrë nga User-i i loguar
+            var result = await _mediator.Send(command);
+            return Ok(new { id = result });
+        }
+    }
+
+}
---
+++ EliteBackend/EliteFit.Domain/Interfaces/Services/IFileStorageService.cs
@@ -0,0 +1,12 @@
+﻿
+using System.IO; // Përdor Stream në vend të IFormFile
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EliteFit.Domain.Interfaces.services
+{
+    public interface IFileStorageService
+    {
+        Task<int> UploadFileAsync(Stream fileStream, string fileName, string folderName, int? uploaderId, string entityName, CancellationToken cancellationToken);
+    }
+}
